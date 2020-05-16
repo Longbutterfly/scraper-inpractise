b261f792c159b335d4644c3a87c4abaf194e1b7b
Longbutterfly <65463535+Longbutterfly@users.noreply.github.com>
2020-05-16 20:20:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-
-url = "https://inpractise.com/articles/n26-disrupting-banking"
-print(requests.get(url).content)
-
-def get_download(url):
-    result = requests.get(url)
-    soup = BeautifulSoup(result.content)
-    download = soup.findall("audio")
-    link = re.search("https://downloads.ctfassets.net/m2k10scfwpz8/^[a-zA-Z0-9_.-]*$.mp3", download)
-    return link
+x = ("Kaas is lekker")
+Print(x)
